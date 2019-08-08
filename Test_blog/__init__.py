eeb49533bd8f55fe8293e45f0c7d583b6e573828
kris_m <303480987@qq.com>
2019-08-08 16:34:15 +0800

    Initial commit

---
+++ Test_blog/__init__.py
@@ -0,0 +1,2 @@
+import pymysql   # 通过pipenv install pymysql  安装
+pymysql.install_as_MySQLdb()
---
+++ myblog/middleware/mymiddleware.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 18-11-15 下午8:20
+# @Author  : Felix Wang
+from django.utils.deprecation import MiddlewareMixin
+from django.shortcuts import render_to_response
+
+
+class My404(MiddlewareMixin):
+    def process_response(self, request, response):
+        if str(response.status_code) == '404':
+            import random
+            return render_to_response(random.choice(['my404/my404_1.html', 'my404/my404_2.html']))
+        return response
